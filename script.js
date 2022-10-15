7cbb51d5a7bce485518b2b9c73279dff8dd6fdce
Yixin88 <yixin88@hotmail.co.uk>
2022-10-15 22:17:58 +0100

    added get discount function if the total order price is greater than 50

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function getCents(price) {
   const splitPrice = price.toString().split(".");
   return splitPrice[1];
 }
+
+function getsDiscount(rec) {
+    let total = 0;
+    for (let i = 0; i < rec.length; i++) {
+      total += rec[i];
+    }
+    if (total > 50) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
