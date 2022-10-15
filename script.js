9cd100c1641114d35cd3bdb0f5207ad81517f669
Yixin88 <yixin88@hotmail.co.uk>
2022-10-15 22:20:23 +0100

    added calculateDiscount function to get the final price

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ function getsDiscount(rec) {
       return 0;
     }
   }
+
+  function calculateTotal(receipt) {
+    let total = 0;
+    let totalSavings = 0;
+    for (let i = 0; i < receipt.length; i++) {
+      let originalPrice = receipt[i];
+      if (getsDiscount(receipt)) {
+        let discountRate = findDiscount(originalPrice);
+        let savings = originalPrice * discountRate;
+        totalSavings += savings;
+        let discountedPrice = originalPrice - savings;
+        total += discountedPrice;
+        if (discountRate !== 0) {
+          console.log(`Item ${i + 1} Savings: $${savings.toFixed(2)}`);
+        }
+      } else {
+        total += originalPrice;
+      }
+    }
+    console.log(`Total Saved: $${totalSavings.toFixed(2)}`);
+    console.log(`Total Post-Discount: $${total.toFixed(2)}`);
+  }
+  calculateTotal(receipt);
